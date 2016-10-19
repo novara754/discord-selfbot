85dd47793b5a9a0fb392daf3926344fe95c5df1b
vzwGrey <thegreylp@googlemail.com>
2016-10-19 18:26:10 +0200

    Changed formatting of eval command

--- commands/eval.js
+++ commands/eval.js
@@ -10,7 +10,6 @@ module.exports = {
     let input = params.join(' ');
 
     try {
-      let message = msg, self = client, bot = client;
       let output = eval(input.replace(/\r?\n|\r/g, ' '));
 
       if(typeof output != 'string') {
@@ -19,10 +18,12 @@ module.exports = {
 
       output = output.replace(client.token, '[token redacted]').replace(client.user.email, '[email redacted]');
 
-      msg.edit(`***\`Input\`*** \`\`\`js\n${input}\n\`\`\`\n***\`Output\`*** \`\`\`js\n${output}\n\`\`\``).catch(console.error);
+      //msg.edit(`***\`Input\`*** \`\`\`js\n${input}\n\`\`\`\n***\`Output\`*** \`\`\`js\n${output}\n\`\`\``).catch(console.error); OLD
+      msg.edit(`${msg.content}\n\`\`\`js\n${output}\n\`\`\``);
 
     } catch (error) {
-      msg.edit(`***\`Input\`*** \`\`\`js\n${input}\n\`\`\`\n***\`Error\`*** \`\`\`js\n${error}\n\`\`\``).catch(console.error);
+      //msg.edit(`***\`Input\`*** \`\`\`js\n${input}\n\`\`\`\n***\`Error\`*** \`\`\`js\n${error}\n\`\`\``).catch(console.error); OLD
+      msg.edit(`${msg.content}\n\`\`\`js\n${error}\n\`\`\``);
     }
   }
 };
