588aeddd1031065124f905d16e2d48aaa175cc5f
vzwGrey <thegreylp@googlemail.com>
2016-10-26 20:35:29 +0200

    Updated formatting

--- commands/help.js
+++ commands/help.js
@@ -6,14 +6,16 @@ module.exports = {
     aliases: ['h']
   },
 
-  exec: (client, msg, [ command_name ]) => {
-    let command = client.commands.get(command_name);
-    let aliases = `Aliases:: ${command.help.aliases.join(', ')}.`;
-    msg.editCode('asciidoc', [
-      `= ${command.help.name} =\n`,
-      `Usage:: ${command.help.name} ${command.help.usage}`,
-      `Description:: ${command.help.desc}`,
+  exec: (client, msg, [ commandName ]) => {
+    let command = client.commands.get(commandName) || client.aliases.get(commandName);
+    let aliases = `Aliases\n= ${command.help.aliases.join(', ')}.`;
+    let help = [
+      `[ ${command.help.name} ]\n`,
+      `Usage\n= ${client.conf.prefix}${command.help.name} ${command.help.usage}\n`,
+      `Description\n= ${command.help.desc}\n`,
       `${command.help.aliases.length > 0 ? aliases : ''}`
-    ]);
+    ];
+
+    msg.edit(`**\`\`\`ini\n${help.join('\n')}\`\`\`**`);
   }
 };
