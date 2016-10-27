0de810c99143bbffad1d186b218906a840b26ebd
vzwGrey <thegreylp@googlemail.com>
2016-10-27 20:21:02 +0200

    Fixed code formatitting

--- commands/list.js
+++ commands/list.js
@@ -9,7 +9,9 @@ module.exports = {
   exec: (client, msg, _) => {
     let list = '';
     let commandNames = Array.from(client.commands.keys());
-    let longest = commandNames.reduce((longest, str) => Math.max(longest, str.length), 0);
+    let longest = commandNames.reduce((longest, str) => {
+      return Math.max(longest, str.length);
+    }, 0);
 
     client.commands.forEach(command => {
       list += `${command.help.name}::${' '.repeat(longest - command.help.name.length)} ${command.help.desc}\n`;
