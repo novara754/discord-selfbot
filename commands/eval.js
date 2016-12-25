ebcdd8692f28e4a7d5240b605cc95f7c9d6741b1
vzwGrey <thegreylp@googlemail.com>
2016-12-25 04:00:03 +0100

    Remove commented-out code

--- commands/eval.js
+++ commands/eval.js
@@ -18,11 +18,9 @@ module.exports = {
 
       output = output.replace(client.token, '[token redacted]').replace(client.user.email, '[email redacted]');
 
-      //msg.edit(`***\`Input\`*** \`\`\`js\n${input}\n\`\`\`\n***\`Output\`*** \`\`\`js\n${output}\n\`\`\``).catch(console.error); OLD
       msg.edit(`${msg.content}\n\`\`\`js\n${output}\n\`\`\``).catch(console.error);
 
     } catch (error) {
-      //msg.edit(`***\`Input\`*** \`\`\`js\n${input}\n\`\`\`\n***\`Error\`*** \`\`\`js\n${error}\n\`\`\``).catch(console.error); OLD
       msg.edit(`${msg.content}\n\`\`\`js\n${error}\n\`\`\``).catch(console.error);
     }
   }
